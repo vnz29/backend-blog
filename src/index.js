bdb25867a8ddb9925daa923ab97f7dd1278256c9
vnz29 <vince.great29@gmail.com>
2025-02-18 17:28:46 +0800

    add home pages

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import routes from './routes/indexRoutes.js'
 const app = express();
 // Connect to MongoDB
 connectDB();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(routes)
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('Hello from Express on Vercel!');
+  });
 
 app.listen(PORT, () =>{
     console.log(`RUNNING IN PORT ${PORT}`)
--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import blogRouter from "./blogRoutes.js"
 const router = Router()
-router.get('/', (req, res) => {
-    res.send("hello deployment")
-})
+// router.get('/', (req, res) => {
+//     res.send("hello deployment")
+// })
 router.use(blogRouter)
 
 export default router
